c57e1c8b7895ccaa9967e93f0d49c47d039b6d32
Sabrina Lucero <sabrinita185@gmail.com>
2020-02-28 08:33:32 -0300

    Update message no results

--- src/screens/AppBar.js
+++
@@ -1,2 +0,0 @@
-import { React } from "react";
-import { makeStyles } from "@material-ui/core/styles";
--- src/screens/search-movie/components/NoResults.js
+++ src/screens/search-movie/components/NoResults.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 
 const NoResults = () => {
-  return <Text>No se encontro un carajo</Text>;
+  return <Text>No results found</Text>;
 };
 
 export default NoResults;
